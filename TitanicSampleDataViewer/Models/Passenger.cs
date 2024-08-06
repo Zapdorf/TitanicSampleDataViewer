ce32d6ffe1af5dfe4b61cc8dcdb36062f721373e
Andrew Vandercar <112919868+Zapdorf@users.noreply.github.com>
2024-08-05 20:50:32 -0500

    Data read and displayed

--- TitanicSampleDataViewer/Models/Passenger.cs
+++ TitanicSampleDataViewer/Models/Passenger.cs
@@ -2,6 +2,17 @@
 {
     public class Passenger
     {
-        public int Id { get; set; }
+        public int PassengerId { get; set; }
+        public bool Survived { get; set; }
+        public int Pclass { get; set; }
+        public string? Name { get; set; }
+        public string? Sex { get; set; }
+        public int Age { get; set; }
+        public int SibSp { get; set; }
+        public int Parch { get; set; }
+        public string? Ticket { get; set; }
+        public double Fare { get; set; }
+        public string? Cabin { get; set; }
+        public char? Embarked { get; set; }
     }
 }
--- TitanicSampleDataViewer/Pages/Index.cshtml.cs
+++ TitanicSampleDataViewer/Pages/Index.cshtml.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using TitanicSampleDataViewer.Models;
+using Dapper;
+using System.Data.Odbc;
 
 namespace TitanicSampleDataViewer.Pages
 {
     public class IndexModel : PageModel
     {
         public List<Passenger> PassengerList { get; set; } = new List<Passenger>();
+
+        //private const string _connectionString = "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=";
         
         private readonly ILogger<IndexModel> _logger;
 
@@ -17,7 +21,28 @@ namespace TitanicSampleDataViewer.Pages
 
         public void OnGet()
         {
-            PassengerList.Add(new Passenger() { Id = 4 });
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "TitanicData.accdb");
+            //string combo = _connectionString + filePath;
+
+            string start = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=";
+            start += filePath;
+
+
+
+            //var connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\SHODAN\Documents\Databases\TitanicData.accdb;";
+            var connectionString = "Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=C:\\Users\\SHODAN\\Documents\\Databases\\TitanicData.accdb;";
+
+            //var connectionString = @$"{start};";
+            using (var connection = new OdbcConnection(connectionString))
+            {
+                // Create a query that retrieves all books with an author name of "John Smith"    
+                var sql = "SELECT * FROM Passengers";
+
+                // Use the Query method to execute the query and return a list of objects    
+                PassengerList = connection.Query<Passenger>(sql).ToList();
+            }
+
+            //PassengerList.Add(new Passenger() { Id = 4 });
             PopulatePassengerListTable();
         }
 
